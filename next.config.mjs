ee872174a1db924452a5d6d6e2d1bc82ab1fa287
Maks Mykolenko <mikolenkomaksim11@gmail.com>
2026-07-27 11:29:44 +0200

    feat: configure static export & GitHub Actions for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? '/GeyClubRomashka' : '',
   images: {
     unoptimized: true,
-    domains: ['images.unsplash.com', 'romashka.club'],
   },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
